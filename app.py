a4d4b9df3d7dfa2464608a18fa6008d79bf104e6
Seyong Kang <nettie0917@gmail.com>
2021-04-12 15:21:36 +0900

    fix

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ class RecoveryManager():
         # Poll kafka
         def poll():
             # Initialize consumer Instance
-            consumer = KafkaConsumer(topic, bootstrap_servers=BOOTSTRAP_SERVERS, auto_offset_reset='earliest', enable_auto_commit=True, 
+            consumer = KafkaConsumer(topic,security_protocol="SSL" , bootstrap_servers=BOOTSTRAP_SERVERS, auto_offset_reset='earliest', enable_auto_commit=True, 
                                         group_id='my-mc' )
 
             print("About to start polling for topic:", topic)
@@ -43,7 +43,7 @@ class RecoveryManager():
 
     def sendkafka(self, topic,data, status):
         data['status'] = status
-        self.producer.send( topic, value=data).add_callback(self.on_send_success).add_errback(self.on_send_error) 
+        self.producer.send( topic, value=data).get()#.add_callback(self.on_send_success).add_errback(self.on_send_error) 
         self.producer.flush() 
 
 
@@ -55,11 +55,10 @@ class RecoveryManager():
         if status == "fail-reduce-kafka-user" or status == "fail-lack-kafka-user" or status == "fail-kafka-user" or status == "fail-kafka-delivery" or status == "fail-kafka-credit":
             url= 'http://flask-product-restapi.flask-product-restapi/product/' + str( json_data['product_id'])
             r = requests.get( url )
-            ret_json = json.loads(r.content)
             if r.status_code != 200:
                 self.sendkafka("retrykafka", json_data, status)   
                 return;      
-
+            ret_json = json.loads(r.content)
             ret_json['count'] += json_data['count']
             ret_json = json.dumps(ret_json)
             r = requests.patch( url ,ret_json)     
@@ -68,15 +67,14 @@ class RecoveryManager():
                 return;      
 
         if status == "fail-kafka-delivery" or status == "fail-kafka-credit":
-            url= 'http://flask-user-restapi:5050/user/' + str(json_data['customer_id'])       
+            url= 'http://flask-user-restapi.flask-user-restapi/user/' + str(json_data['customer_id'])       
             r = requests.get( url )
             if r.status_code != 200:
                 self.sendkafka("retrykafka", json_data, status)   
-                return;     
-            t_count = json_data['count']
-            t_price = json_data['price']
-            ret_json['money'] += t_count * t_price
-            ret_json = json.dumps(ret_json)
+                return; 
+            ret_json = json.loads(r.content)            
+            ret_json['money'] += json_data['count'] * json_data['price']
+            ret_json = json.dumps(ret_json)            
             r = requests.patch( url ,ret_json) 
             if r.status_code != 200:
                 self.sendkafka("retrykafka", json_data, status)   
