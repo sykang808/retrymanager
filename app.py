f1f17aaa9ea49a62b86d5aa6c5e60262fcbfe453
Seyong Kang <nettie0917@gmail.com>
2021-05-25 00:49:11 +0900

    fix utf8

--- app.py
+++ app.py
@@ -121,3 +121,33 @@ if __name__ == '__main__':
     productmanager3.register_kafka_listener('recoverykafka')
     productmanager4 = RecoveryManager()
     productmanager4.register_kafka_listener('recoverykafka')            
+
+
+
+
+from kafka import KafkaProducer 
+from json import dumps 
+import time 
+
+producer = KafkaProducer(bootstrap_servers=BOOTSTRAP_SERVERS, security_protocol="SSL", value_serializer=lambda x: dumps(x).encode('utf-8'))    
+start = time.time() 
+
+for i in range(10000): 
+    data = {'str' : 'result'+str(i)} 
+    producer.send('test', value=data) 
+    producer.flush() 
+
+print("elapsed :", time.time() - start)
+
+
+
+from kafka import KafkaConsumer 
+from json import loads 
+# topic, broker list 
+consumer = KafkaConsumer( 'test', bootstrap_servers=BOOTSTRAP_SERVERS, auto_offset_reset='earliest', enable_auto_commit=True, group_id='my-group', value_deserializer=lambda x: loads(x.decode('utf-8')), consumer_timeout_ms=1000 ) 
+
+# consumer list를 가져온다 
+print('[begin] get consumer list') 
+for message in consumer: 
+    print("Topic: %s, Partition: %d, Offset: %d, Key: %s, Value: %s" % ( message.topic, message.partition, message.offset, message.key, message.value )) 
+print('[end] get consumer list')
